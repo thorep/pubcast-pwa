b3a21900f4dcc9a5b7527b0ffaf4e7236e02f3b4
Thor Prestbøen <thor.erling.prestboen@gmail.com>
2025-12-27 18:21:38 +0100

    Autoupdate

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    registerType: "prompt",
+    registerType: "autoUpdate",
     injectRegister: false,
 
     pwaAssets: {
