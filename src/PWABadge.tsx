1d1d58318412a8485e490b6ba8ef4526c49b6de3
Thor Prestbøen <thor.erling.prestboen@gmail.com>
2025-12-27 11:52:26 +0100

    Auto update for app

--- src/PWABadge.tsx
+++ src/PWABadge.tsx
@@ -2,7 +2,9 @@ import { useRegisterSW } from 'virtual:pwa-register/react'
 
 function PWABadge() {
   // check for updates every hour
-  const period = 60 * 60 * 1000
+
+ // const period = 60 * 60 * 1000
+  const period = 1000*5
 
   const {
     
